bdbb9e79e91107ed9916578a2d4f1f2a7a142b42
pridato <davidarroyo25052004@gmail.com>
2024-06-19 19:06:56 +0200

    react uso de ts

--- src/components/proyects/project-card.tsx
+++ src/components/proyects/project-card.tsx
@@ -1,5 +1,5 @@
 import { Tag } from '../../enums/tags';
-import Svelte from "./../techs-buttons/svelte";
+import Svelte from "../techs-buttons/svelte";
 import Angular from "../techs-buttons/angular";
 import React from "../techs-buttons/react";
 import Stripe from "../techs-buttons/stripe";
@@ -8,8 +8,9 @@ import Astro from '../techs-buttons/astro';
 import Paypal from '../techs-buttons/paypal';
 import Node from '../techs-buttons/node';
 import Firebase from '../techs-buttons/firebase';
+import type { Project } from '../../models/project';
 
-const renderTagComponents = (tag) => {
+const renderTagComponents = (tag:any) => {
   switch (tag) {
     case Tag.Angular:
       return <Angular />;
@@ -34,7 +35,7 @@ const renderTagComponents = (tag) => {
   }
 };
 
-const ProjectCard = ({ name, description, tags, github_url, image_url }) => {
+const ProjectCard = ({ name, description, tags, github_url, image_url }:Project) => {
   return (
     <div className="project-card-sm">
       <div className="card-border-gradient"></div>
@@ -44,7 +45,7 @@ const ProjectCard = ({ name, description, tags, github_url, image_url }) => {
           <h1>{name}</h1>
           <p>{description}</p>
           <div className="flex items-center justify-center gap-x-5 absolute top-5 left-5">
-            {tags.map((tag, index) => (
+            {tags.map((tag:any, index:any) => (
               <div key={index} className="tag-item">
                 {renderTagComponents(tag)}
               </div>
@@ -52,7 +53,7 @@ const ProjectCard = ({ name, description, tags, github_url, image_url }) => {
           </div>
           {/* enlace github del proyecto*/}
           <a href={github_url} className='p-2' target='_blank'>
-            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-github" width="52" height="52" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-brand-github" width="52" height="52" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
               <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
               <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" />
               <title>github</title>
